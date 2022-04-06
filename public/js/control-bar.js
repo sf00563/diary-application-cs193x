f7058ae2c5477c254501760a53e27e9b00d9ad06
Sam Foster <samf2398@gmail.com>
2022-04-06 15:00:01 +0100

    added in functionality for updating an entry and changing controls

--- public/js/control-bar.js
+++ public/js/control-bar.js
@@ -7,16 +7,21 @@ class ControlBar {
 
     const backImage = document.createElement('img');
     backImage.src = './img/back.png';
-    const backButton = document.createElement('button');
-    backButton.append(backImage);
+    this.backButton = document.createElement('button');
+    this.backButton.append(backImage);
 
     const forwardImage = document.createElement('img');
     forwardImage.src = './img/forward.png';
-    const forwardButton = document.createElement('button');
-    forwardButton.append(forwardImage);
+    this.forwardButton = document.createElement('button');
+    this.forwardButton.append(forwardImage);
 
-    this.containerElement.append(backButton);
+    this.containerElement.append(this.backButton);
     this.containerElement.append(home);
-    this.containerElement.append(forwardButton);
+    this.containerElement.append(this.forwardButton);
+  }
+
+  changeToAcceptContols() {
+    this.forwardButton.classList.add('hidden');
+    this.backButton.classList.add('hidden');
   }
 }
--- public/js/diary-entry.js
+++ public/js/diary-entry.js
@@ -3,12 +3,63 @@ class DiaryEntry {
   constructor(diaryId, containerElement) {
     this.containerElement = containerElement;
     this.diaryId = diaryId;
+    this.currentDate = new Date();
+    this.title = document.querySelector('.entry h1');
+    this.textArea = document.querySelector('textarea');
+    this.paragraph = document.querySelector('.entry-details');
+
+    this.showTextArea = this.showTextArea.bind(this);
+    this.updateEntry = this.updateEntry.bind(this);
+
+    const options = { month: 'long', day: 'numeric' };
+
+    this.title.textContent = this.currentDate.toLocaleDateString('en-US', options)
 
     const controlBarContainer = document.querySelector('.control-bar');
-    const controlBar = new ControlBar(controlBarContainer);
+    this.controlBar = new ControlBar(controlBarContainer);
 
-    console.log(diaryId);
+    const textContainer = document.querySelector('.text-container');
+    textContainer.addEventListener("click", this.showTextArea);
+
+    const entryScreen = document.querySelector('.entry');
+    entryScreen.addEventListener("click", this.updateEntry);
+
+    this.loadEntry();
     this.containerElement.classList.remove('hidden');
   }
+
+  loadEntry() {
+    const objectr = {
+      id: 'sfdhkjfdskjhjhkdfs',
+      diaryId: '87345874358',
+    }
+
+    if (objectr.content) {
+      this.paragraph.textContent = objectr.content;
+      this.paragraph.classList.remove('hidden');
+    }
+  }
+
+  showTextArea(event) {
+    event.stopPropagation();
+    this.paragraph.classList.add('hidden');
+    this.textArea.textContent = this.paragraph.textContent;
+    this.controlBar.changeToAcceptContols();
+    this.textArea.classList.remove('hidden');
+  }
+
+  updateEntry() {
+    if (!this.textArea.classList.contains('hidden')) {
+      const text = this.textArea.value;
+      console.log(text);
+      if (this.paragraph.textContent !== text) {
+        console.log("update on db");
+        this.paragraph.textContent = text
+      }
+      this.textArea.classList.add('hidden');
+      this.paragraph.classList.remove('hidden');
+    }
+  }
+
 
 }
