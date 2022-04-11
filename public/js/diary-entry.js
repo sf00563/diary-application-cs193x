1fcfdc7c2663694e34a894ad248e55dde61812ab
Sam Foster <samf2398@gmail.com>
2022-04-11 14:31:06 +0100

    added in prompts and database test script

--- public/js/diary-entry.js
+++ public/js/diary-entry.js
@@ -5,8 +5,40 @@ class DiaryEntry {
     this.diaryId = diaryId;
     this.date = '';
     this.title = document.querySelector('.entry h1');
+    this.prompt = document.querySelector('.entry p');
     this.textArea = document.querySelector('textarea');
     this.paragraph = document.querySelector('.entry-details');
+    this.prompts = ['List the things that make you feel powerful.',
+      'What is something that made you laugh today?',
+      'List the movies that you want to watch.',
+      'List the things that make you feel peaceful.',
+      'List your greatest comforts.',
+      'What is something that brightens your day?',
+      'List three things you accomplished today.',
+      'What is something you look forward to every day?',
+      'What is a game that you like to play?',
+      'What is your Sunday ritual?',
+      'List the most memorable moments of this month so far.',
+      'List some things you want to do outdoors.',
+      'If you could live anywhere you wanted, where would you live?',
+      'List what you would spend a million dollars on, just for you.',
+      'When do you feel most energized?',
+      'List the things that make you feel excited.',
+      'List your favorite snacks or treats.',
+      'What has you busy this week?',
+      'List the people you admire.',
+      'List the happiest moments of your year so far.',
+      'What hobby would you like to pick up?',
+      'List the ways you love to have fun.',
+      'Describe something you learned today',
+      'List something fun you did or will do today.',
+      'What is your dream job?',
+      'List the things that inspire you.',
+      'List something you did today that you are proud of.',
+      'Find a quote that you like and write it down here.',
+      'List something you should ignore.',
+      'Talk about something you are excited about next month.',
+      'List three traits you would like others to see in you.']
 
     this.showTextArea = this.showTextArea.bind(this);
     this.updateEntry = this.updateEntry.bind(this);
@@ -26,13 +58,24 @@ class DiaryEntry {
   }
 
   loadEntry(value) {
+    const options = { month: 'long', day: 'numeric' };
+
     if (value === 0) {
       this.date = new Date();
     } else {
       this.date.setDate(this.date.getDate() + value);
     }
-    const options = { month: 'long', day: 'numeric' };
+
+    const day = this.date.toLocaleDateString().substring(0, 2);
+    let index = 0;
+    if (day.charAt(0) === 0) {
+      index = parseInt(day.charAt(1));
+    } else {
+      index = parseInt(day);
+    }
+
     this.title.textContent = this.date.toLocaleDateString('en-US', options);
+    this.prompt.textContent = this.prompts[index - 1];
 
     const objectr = {
       id: 'sfdhkjfdskjhjhkdfs',
--- test-server.js
+++ test-server.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require("mongodb");
+const ObjectID = require('mongodb').ObjectId;
 
 const DATABASE_NAME = 'diary-entry';
 const MONGO_URL = `mongodb://localhost:27017/${DATABASE_NAME}`;
@@ -15,15 +16,55 @@ async function main() {
   console.log('Insert Diary');
   await insertDiary();
   console.log('--------------');
+
+  console.log('Find Entry');
+  await findEntry();
+  console.log('--------------');
+
+  console.log('Insert or update Entry');
+  await insertOrUpdateEntry();
+
+  db.close();
 }
 
 main();
 
 async function insertDiary() {
+  const date = new Date();
   const doc = {
-    entryCount: 0
+    dateCreated: date.toLocaleDateString()
   };
 
   const result = await collectionDiaries.insertOne(doc);
   console.log(`Document id: ${result.insertedId}`);
+}
+
+async function findEntry() {
+  const curDate = new Date();
+  const query = {
+    diaryId: ObjectID('12EUFSDUJFDSKJDSJK'),
+    date: curDate.toLocaleDateString()
+  };
+
+  const result = await collectionDiaryEntries.findOne(query);
+  console.log(`Found: ${result}`);
+}
+
+async function insertOrUpdateEntry() {
+  const query = {
+    _id: ObjectID('14238345rfjsdmndsf8348345')
+  };
+
+  const newEntry = {
+    diaryId: ObjectID('12EUFSDUJFDSKJDSJK'),
+    date: curDate.toLocaleDateString(),
+    contents: "Peter parker"
+  }
+
+  const params = {
+    upsert: true
+  }
+
+  const result = await collectionDiaryEntries.update(query, newEntry, params);
+  console.log(`Found: ${result}`);
 }
